d901861d2ebaf39db0194f1c2c4ce4e4ce73e80d
Konstantin <konstpavlovsdr@gmail.com>
2017-02-18 20:11:53 +0300

    add mapping for adding news

--- src/main/java/ru/konstpavlov/newsline/controller/AppController.java
+++ src/main/java/ru/konstpavlov/newsline/controller/AppController.java
@@ -1,14 +1,68 @@
 package ru.konstpavlov.newsline.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import ru.konstpavlov.newsline.entity.Category;
+import ru.konstpavlov.newsline.entity.HeadLine;
+import ru.konstpavlov.newsline.service.HeadLineService;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class AppController {
 
+    HeadLineService headLineService;
+    List<Category> categoryList = new ArrayList<>();
+
+
+    @Autowired
+    public AppController(HeadLineService headLineService) {
+        this.headLineService = headLineService;
+        categoryList.add(Category.MAIN_NEWS);
+        categoryList.add(Category.POLITICS);
+        categoryList.add(Category.SOCIETY);
+        categoryList.add(Category.ECONOMY);
+        categoryList.add(Category.SPORT);
+        categoryList.add(Category.INCIDENTS);
+        categoryList.add(Category.CULTURE);
+        categoryList.add(Category.SCIENCE);
+        categoryList.add(Category.AUTOMOBILES);
+    }
+
     @RequestMapping("/")
-    public String index(Model model){
-        return "index";
+    public ModelAndView index(Model model){
+
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("categories",categoryList);
+        modelAndView.addObject("headLines",headLineService.getAllHeadLine());
+
+        return modelAndView;
+    }
+
+    @RequestMapping("/add")
+    public ModelAndView addHeadLine(@ModelAttribute("headline") HeadLine headline){
+
+        ModelAndView modelAndView = new ModelAndView("addHeadLine");
+        modelAndView.addObject("categories",categoryList);
+        modelAndView.addObject("headline",new HeadLine());
+        return modelAndView;
+    }
+
+
+    @RequestMapping(value = "/addNews", method = RequestMethod.POST)
+    public ModelAndView addNewHeadLine(@ModelAttribute("headline") HeadLine headline){
+        headline.setDate(new Date());
+        headLineService.addHeadLine(headline);
+        String message = "You successfully added a: "+ headline.getTitle();
+        return new ModelAndView("redirect:/","addMessage", message) ;
     }
 }
