083990b14ce8e22d7705863ade20ec3d5701c38c
Konstantin <konstpavlovsdr@gmail.com>
2017-02-18 20:36:57 +0300

    add toString for Enum

--- src/main/java/ru/konstpavlov/newsline/entity/Category.java
+++ src/main/java/ru/konstpavlov/newsline/entity/Category.java
@@ -1,5 +1,21 @@
 package ru.konstpavlov.newsline.entity;
 
 public enum  Category {
-    MAIN_NEWS,POLITICS,SOCIETY,ECONOMY,SPORT,INCIDENTS,CULTURE,SCIENCE,AUTOMOBILES
+    MAIN_NEWS,POLITICS,SOCIETY,ECONOMY,SPORT,INCIDENTS,CULTURE,SCIENCE,AUTOMOBILES;
+
+    @Override
+    public String toString(){
+        switch (this){
+            case MAIN_NEWS: return "Main news";
+            case POLITICS: return "Politics";
+            case SOCIETY: return "Society";
+            case ECONOMY: return "Economy";
+            case SPORT: return "Sport";
+            case INCIDENTS: return "Incidents";
+            case CULTURE: return "Culture";
+            case SCIENCE: return "Science";
+        default:
+            return "Automobiles";
+        }
+    }
 }
